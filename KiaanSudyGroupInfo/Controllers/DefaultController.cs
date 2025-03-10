89baa7549d220d7046450b9295873b5a06e5b172
KiaanIT <u24600904@tuks.co.za>
2025-03-10 23:54:00 +0200

    Final

---
+++ KiaanSudyGroupInfo/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KiaanSudyGroupInfo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult MyIndex()
+        {
+
+            List<Models.MyClass> Support = new List<Models.MyClass>();
+
+            Support.Add(new Models.MyClass { StudentNumber = "u24600904", Name = "Kiaan", Surname = "Taljaard", Email = "u24600904@tuks.co.za", myLink = Url.Content("~/HTML/Kiaan.html") });
+            Support.Add(new Models.MyClass { StudentNumber = "u24943802", Name = "Danel", Surname = "Steyn", Email = "u24943802@tuks.co.za", myLink = Url.Content("~/HTML/Danel.html") });
+            Support.Add(new Models.MyClass { StudentNumber = "u24589412", Name = "Arno", Surname = "Redpath", Email = "u24589412@tuks.co.za" , myLink = Url.Content("~/HTML/Arno.html") });
+            Support.Add(new Models.MyClass { StudentNumber = "u24569748", Name = "Milos", Surname = "Sarovic", Email = "u24569748@tuks.co.za", myLink = Url.Content("~/HTML/Milos.html") });
+            Support.Add(new Models.MyClass { StudentNumber = "u23815524", Name = "Sechaba", Surname = "Phahlamohlaka", Email = "u23815524@tuks.co.za" , myLink = Url.Content("~/HTML/Sechaba.html") });
+            
+
+            return View(Support);
+        }
+    }
+}
--- KiaanSudyGroupInfo/Models/MyClass.cs
+++ KiaanSudyGroupInfo/Models/MyClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,21 @@ namespace KiaanSudyGroupInfo.Models
 {
     public class MyClass
     {
-        string Kiaan;
+
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+
+        [Display(Name = "Email Adress")]
+        public string Email { get; set; }
+
+        [Display(Name = "Link")]
+        public string myLink { get; set; }
     }
 }
